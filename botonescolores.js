3d79dcaf60e9d2e878863cb9ef4b27daee07fcd1
Daniel Villacres <davc_jw@hotmail.com>
2022-07-14 12:30:04 -0500

    ejercicios sencillos

---
+++ botonescolores.js
@@ -0,0 +1,16 @@
+
+function cambiocolor(){
+   document.body.style.backgroundColor="red";
+}
+function azul(){
+    document.body.style.backgroundColor="blue";
+ }
+function green(){
+    document.body.style.backgroundColor="green";
+ }
+function pink(){
+    document.body.style.backgroundColor="pink";
+ }
+ function purple(){
+    document.body.style.backgroundColor="purple";
+ }
